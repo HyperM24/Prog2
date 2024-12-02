771eb5ae617eb256d81826a15a7351eb1cc7e10e
Sir Kanalas Leonárdó <82200593+HyperM24@users.noreply.github.com>
2024-12-02 21:29:56 +0100

    Add files via upload

---
+++ C#_hazi/FizzBuzz/FizzBuzz/Fizzbuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Fizzbuzz
+    {
+
+        private int limit;
+
+        public Fizzbuzz(int limit)
+        {
+            this.limit = limit;
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < limit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+
+    }
+}
---
+++ C#_hazi/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,11 @@
+﻿using FizzBuzz;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Fizzbuzz fb = new Fizzbuzz(100);
+        fb.Start();
+
+    }
+}
