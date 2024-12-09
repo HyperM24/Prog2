0b8a21a0642dd7e91506bb1a760a83686ee4ff4b
Sir Kanalas Leonárdó <82200593+HyperM24@users.noreply.github.com>
2024-12-09 23:25:50 +0100

    Add files via upload

---
+++ C#_Hazi2/Pozitiv_egesz/Pozitiv_egesz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int sum = Enumerable.Range(1, 999)
+                            .Where(n => n % 3 == 0 || n % 5 == 0) 
+                            .Sum(); 
+
+        Console.WriteLine("A 1000-nél kisebb számok összege, melyek 3-nak vagy 5-nek a többszörösei: " + sum);
+    }
+}
