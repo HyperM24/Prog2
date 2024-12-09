d6b93e4df98f7986e05d28823e599f2b6bcbcbda
Sir Kanalas Leonárdó <82200593+HyperM24@users.noreply.github.com>
2024-12-09 23:26:18 +0100

    Add files via upload

---
+++ C#_Hazi2/Reverse/Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine(ReverseNumber(1977)); 
+        Console.WriteLine(ReverseNumber(12568)); 
+    }
+
+    static int ReverseNumber(int number)
+    {
+        int reversed = 0;
+
+        while (number > 0)
+        {
+           
+            int digit = number % 10;
+
+            
+            reversed = reversed * 10 + digit;
+
+            
+            number /= 10;
+        }
+
+        return reversed;
+    }
+}
