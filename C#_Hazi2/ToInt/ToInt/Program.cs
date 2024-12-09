67d94647a61fe08cc486b4c7bce0b135fd7d07c0
Sir Kanalas Leonárdó <82200593+HyperM24@users.noreply.github.com>
2024-12-09 23:27:21 +0100

    Add files via upload

---
+++ C#_Hazi2/ToInt/ToInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static int ToInt(this string s)
+    {
+      
+        return int.Parse(s);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string s = "42";
+
+        int i = s.ToInt();
+
+        Console.WriteLine(s);    
+        Console.WriteLine(i);    
+    }
+}
